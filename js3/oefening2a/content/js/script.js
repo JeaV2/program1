ee8108f747b3bf6ed2df12cb78ca43fa2754eedd
JeaV2 <102710@glr.nl>
2025-01-09 09:43:29 +0100

    Oefening 2A

---
+++ js3/oefening2a/content/js/script.js
@@ -0,0 +1,7 @@
+let globaleVariabele = "Dit is een globale variabele";
+
+function test() {
+    let lokaleVariabele = "Dit is een lokale variabele";
+    console.log(globaleVariabele);
+    console.log(lokaleVariabele);
+}
