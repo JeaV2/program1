3450262323d75aa5b7aaaf8eeb08170ebe45a9a8
JeaV2 <102710@glr.nl>
2025-01-14 11:17:56 +0100

    Oefening 2H

---
+++ js3/oefening2h/js/debug.js
@@ -0,0 +1,6 @@
+// Je kan de variable en function niet dezelfde naam geven. en de functie "myVar" bestaat niet.
+let myVariable = 1;
+let add = function(a,b){
+    return a+b;
+}
+console.log(add(1,2));
