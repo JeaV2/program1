354ccdb5af32007fc56d30942bbea8f755549abb
JeaV2 <102710@glr.nl>
2025-01-21 11:06:21 +0100

    Opdracht 3

---
+++ js3/opdracht3/content/js/sounds.js
@@ -0,0 +1,24 @@
+// Define variables for the pop sfx
+const pop1 = new Audio('content/sfx/pop1.mp3');
+const pop2 = new Audio('content/sfx/pop2.mp3');
+const pop3 = new Audio('content/sfx/pop3.mp3');
+const pop4 = new Audio('content/sfx/pop4.mp3');
+
+// array with pop sfx
+const popSounds = [pop1, pop2, pop3, pop4];
+
+// play pop sfx when a, s, d or f keys are pressed
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'a'){
+        popSounds[0].play();
+    }
+    if (event.key === 's'){
+        popSounds[1].play();
+    }
+    if (event.key === 'd'){
+        popSounds[2].play();
+    }
+    if (event.key === 'f'){
+        popSounds[3].play();
+    }
+});
