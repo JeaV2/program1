fb37487afd466dce8dc79632d833862d8b94dbdf
JeaV2 <102710@glr.nl>
2025-03-11 10:53:49 +0100

    Oefening 3

---
+++ php1/oefening3/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$string = "Hier tellen we twee getallen op";
+$getal1 = 10;
+$getal2 = 22.2;
+
+include_once("view.php");
---
+++ php1/oefening3/view.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oefening 3 | view</title>
+</head>
+<body>
+    <p>
+        <?= $string ?>
+    </p>
+    <p>
+        Getal 1: <?= $getal1 ?>
+    </p>
+    <p>
+        Getal 2: <?= $getal2 ?>
+    </p>
+    <p>
+        De som van getal 1 en 2 is: <?= $getal1 + $getal2 ?>
+    </p>
+</body>
+</html>
