9b5f34dae41260382eff360b3da24cae5c9f48ba
JeaV2 <102710@glr.nl>
2025-03-25 11:30:51 +0100

    Oefening 7.2

--- php1/oefening7/index_view.php
+++ php1/oefening7/index_view.php
@@ -10,24 +10,30 @@
 <body>
     <h1>Formulier</h1>
     <form action="verwerk.php" method="post">
+
+
+        <?php if (isset($errors)){ ?>
+            <p><?= @$errors['naam'] ?></p>
+        <?php } ?>
         <label for="naam">Wat is je naam?:</label>
         <input type="text" name="naam" id="naam" >
         <br>
+
+        <?php if (isset($errors)){ ?>
+            <p><?= @$errors['email'] ?></p>
+        <?php } ?>
         <label for="email">Wat is je email-adres?:</label>
         <input type="email" name="email" id="email" >
         <br>
+
+        <?php if (isset($errors)){ ?>
+            <p><?= @$errors['leeftijd'] ?></p>
+        <?php } ?>
         <label for="leeftijd">Wat is je leeftijd?:</label>
         <input type="number" name="leeftijd" id="leeftijd" >
         <br>
         <br>
         <input type="submit" value="submit">
     </form>
-    <?php if (isset($errors)){ ?>
-        <ul>
-            <?php foreach ($errors as $error){ ?>
-                <li><?= $error; ?></li>
-            <?php } ?>
-        </ul>
-    <?php } ?>
 </body>
 </html>
